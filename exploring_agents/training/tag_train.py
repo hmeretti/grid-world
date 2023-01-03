ec9599235d43127a62a47c7cc91da7ffc3efe253
hmeretti <hmeretti@gmail.com>
2023-01-03 17:06:14 -0300

    improving tag training

--- exploring_agents/training/tag_train.py
+++ exploring_agents/training/tag_train.py
@@ -2,6 +2,7 @@ from abstractions import Agent
 from grid_world.action import GWorldAction
 from grid_world.grid_world import GridWorld
 from grid_world.state import TagState, GWorldState
+from utils.operations import add_tuples
 from utils.returns import returns_from_reward
 
 
@@ -20,52 +21,83 @@ def run_tag_episode(
     list[float],
     list[GWorldAction],
 ]:
-    state_1 = initial_state_1 if initial_state_1 is not None else world.initial_state
-    state_2 = initial_state_2 if initial_state_2 is not None else world.initial_state_2
+    state_1_t0 = initial_state_1 if initial_state_1 is not None else world.initial_state
+    state_2_t0 = initial_state_2 if initial_state_2 is not None else world.initial_state_2
 
-    agent_1_states = [state_1]
+    agent_1_states = [state_1_t0]
     agent_1_rewards = []
     agent_1_actions = []
 
-    agent_2_states = [state_2]
+    agent_2_states = [state_2_t0]
     agent_2_rewards = []
     agent_2_actions = []
 
     # run through the world while updating q and the policy as we go
     effect_1 = 0
     effect_2 = 0
+    next_action_1 = None
     t = 0
+
+    # lets predefine this to make life easier
+    initial_state_t0 = TagState(state_1_t0.coordinates, state_2_t0.coordinates)
+    action_1_t0 = agent_1.select_action(initial_state_t0) if next_action_1 is None else next_action_1
+    state_1_t1, effect_1_t0 = world.take_action(state_1_t0, action_1_t0)
+
     while effect_1 != 1 and effect_2 != 1:
-        # agent 1 taking action
-        state = TagState(state_1.coordinates, state_2.coordinates)
-        action_1 = agent_1.select_action(state)
-        next_state_1, effect_1 = world.take_action(state_1, action_1)
-        if next_state_1 == state_2:
-            effect_1 = 1
-
-        next_state = TagState(next_state_1.coordinates, state_2.coordinates)
-        reward_1 = agent_1.run_update(state, action_1, effect_1, next_state)
-
+        # agent 1 behaviour is predetermined
         # agent 2 taking action
-        action_2 = agent_2.select_action(state)
-        next_state_2, effect_2 = world.take_action(state_2, action_2)
-        if t == episode_max_length - 1:
-            effect_2 = 1
+        intermediate_state_t0 = TagState(state_1_t1.coordinates, state_2_t0.coordinates)
+        action_2_t0 = agent_2.select_action(intermediate_state_t0)
+        state_2_t1, effect_2_t0 = world.take_action(state_2_t0, action_2_t0)
+        initial_state_t1 = TagState(state_1_t1.coordinates, state_2_t1.coordinates)
 
-        final_state = TagState(next_state_1.coordinates, next_state_2.coordinates)
-        reward_2 = agent_2.run_update(state, action_2, effect_2, final_state)
+        # determine agent 1 reward
+        # if agent 1 moves to agent 2 position or agent 2 moves to agent 1 position episode ends, 1 gets reward
+        if state_1_t1 == state_2_t0 or state_1_t1 == state_2_t1:
+            effect_1 = 1
+        # if agent 2 survived long enough agent 1 gets punished
+        elif t == episode_max_length - 1:
+            effect_1 = -1
+        else:
+            effect_1 = effect_1_t0
 
-        state_1 = next_state_1
-        state_2 = next_state_2
-        t += 1
+        # update agent 1
+        reward_1 = agent_1.run_update(initial_state_t0, action_1_t0, effect_1, initial_state_t1)
 
-        agent_1_states.append(state_1)
+        # we need to know what state agent 2 will have at t1 in order to update his function
+        # for this we need to already determine what agent 1 will do at t1
+        action_1_t1 = agent_1.select_action(initial_state_t1)
+        state_1_t2, effect_1_t1 = world.take_action(state_1_t1, action_1_t1)
+        intermediate_state_t1 = TagState(state_1_t2, state_2_t1)
+
+        # determine agent 2 reward
+        # if agent 2 moved to agent 1 position or agent 1 manged to move to agent 2 position, agent 2 gets punished
+        if state_1_t1 == state_2_t1 or state_1_t2 == state_2_t1:
+            effect_2 = -1
+        # if agent 2 survived long enough gets rewarded
+        elif t == episode_max_length - 1:
+            effect_1 = 1
+        else:
+            effect_2 = effect_2_t0
+
+        reward_2 = agent_2.run_update(intermediate_state_t0, action_2_t0, effect_2, intermediate_state_t1)
+
+        # store results
+        agent_1_states.append(state_1_t1)
         agent_1_rewards.append(reward_1)
-        agent_1_actions.append(action_1)
+        agent_1_actions.append(action_1_t0)
 
-        agent_2_states.append(state_2)
+        agent_2_states.append(state_2_t1)
         agent_2_rewards.append(reward_2)
-        agent_2_actions.append(action_2)
+        agent_2_actions.append(action_2_t0)
+
+        # update states and preselected actions for next step
+        state_1_t0 = state_1_t1
+        state_2_t0 = state_2_t1
+        initial_state_t0 = TagState(state_1_t0.coordinates, state_2_t0.coordinates)
+        action_1_t0 = action_1_t1
+        state_1_t1 = state_1_t2
+        t += 1
 
     agent_1_returns = returns_from_reward(agent_1_rewards, agent_1.gamma)
     agent_2_returns = returns_from_reward(agent_2_rewards, agent_2.gamma)
